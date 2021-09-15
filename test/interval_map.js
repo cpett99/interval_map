d06d54caeb505bda84443c4a4435c61edb0cfec9
cpett99 <cpett99@gmail.com>
2021-09-15 16:35:35 -0700

    test instructions

--- test/interval_map.js
+++ test/interval_map.js
@@ -9,6 +9,8 @@ myMap.insert([1,6], 4)
 myMap.insert([5,10], 8)
 myMap.insert([12,16], 14)
 myMap.insert([14,22], 68)
+// Add new range
+// myMap.insert([KVALUE_MIN,KVALUE_MAX], MAPPED_VALUE)
 
 
 describe("Interval Map ranges\n    myMap.insert([1,6], 4)\n    myMap.insert([5,10], 8)\n    myMap.insert([12,16], 14)\n    myMap.insert([14,22], 68)\n", function() {
@@ -43,5 +45,15 @@ describe("Interval Map ranges\n    myMap.insert([1,6], 4)\n    myMap.insert([5,1
             expect(myMap.at(14)).to.equal(68);
         });
     });
+
+    // Add new test case
+    /*
+    describe("Find range value for ENTER_RANGE_VALUE", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(ENTER_RANGE_VALUE)).to.equal(ENTER_EXPECTED_RESULT_VALUE);
+        });
+    });
+    */
   
   });
