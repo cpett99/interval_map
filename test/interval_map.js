a6dfdaee198370cc5f274f4184eaf1728c2e6e7a
cpett99 <cpett99@gmail.com>
2021-09-15 14:44:10 -0700

    add test cases with nodejs and mocha

---
+++ test/interval_map.js
@@ -0,0 +1,47 @@
+var expect    = require("chai").expect;
+var mapRanges = require("../app/interval_map")
+
+// Instantiate mapRanges class
+const myMap = new mapRanges()
+
+// set ranges
+myMap.insert([1,6], 4)
+myMap.insert([5,10], 8)
+myMap.insert([12,16], 14)
+myMap.insert([14,22], 68)
+
+
+describe("Interval Map ranges\n    myMap.insert([1,6], 4)\n    myMap.insert([5,10], 8)\n    myMap.insert([12,16], 14)\n    myMap.insert([14,22], 68)\n", function() {
+
+    describe("Find range value for 1", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(1)).to.equal(4);
+        });
+    });
+    describe("Find range value for 5.5", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(5.5)).to.equal(8);
+        });
+    });
+    describe("Find range value for 11", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(11)).to.equal(0);
+        });
+    });
+    describe("Find range value for 13", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(13)).to.equal(14);
+        });
+    });
+    describe("Find range value for 14", function() {
+        // specification for finding range value
+        it("", function() {
+            expect(myMap.at(14)).to.equal(68);
+        });
+    });
+  
+  });
